f81f0c2aa1fc192df0de978974c72bd6a24a9981
bronya0 <1335968147@qq.com>
2021-06-04 20:53:41 +0800

    使用mybatis-generator创建代码

--- src/main/java/com/example/mywiki/MyWikiApplication.java
+++ src/main/java/com/example/mywiki/MyWikiApplication.java
@@ -1,11 +1,13 @@
 package com.example.mywiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.example")
 @SpringBootApplication
+@MapperScan("com.example.mywiki.mapper")
 public class MyWikiApplication {
 
     public static void main(String[] args) {
