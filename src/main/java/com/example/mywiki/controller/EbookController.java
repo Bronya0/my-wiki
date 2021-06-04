4c121385796be89f66c670fcb5843520f990c886
bronya0 <1335968147@qq.com>
2021-06-04 21:34:55 +0800

    定义了通用返回对象CommonResp; 编写了EbookController测试.

--- src/main/java/com/example/mywiki/controller/EbookController.java
+++ src/main/java/com/example/mywiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.example.mywiki.controller;
 
 import com.example.mywiki.domain.Ebook;
+import com.example.mywiki.response.CommonResp;
 import com.example.mywiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public Ebook list(Long id){
-        return ebookService.list(id);
+    public CommonResp list(Long id){
+        CommonResp<Ebook> response = new CommonResp<>();
+        Ebook list = ebookService.list(id);
+        response.setContent(list);
+        return response;
     }
 
 }
