56b13bf05a9f7f07ea9e2446bdefac48ed3acafa
bronya0 <1335968147@qq.com>
2021-06-06 17:27:44 +0800

    1-2 封装返回参数

--- src/main/java/com/example/mywiki/service/EbookService.java
+++ src/main/java/com/example/mywiki/service/EbookService.java
@@ -3,12 +3,16 @@ package com.example.mywiki.service;
 import com.example.mywiki.domain.Ebook;
 import com.example.mywiki.mapper.EbookMapper;
 import com.example.mywiki.request.EbookReq;
+import com.example.mywiki.response.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 电子书服务
  * Created by tangssst@qq.com on 2021/06/04
  */
 @Service
@@ -20,7 +24,15 @@ public class EbookService {
         return ebookMapper.selectByPrimaryKey(req.getId());
     }
 
-    public List<Ebook> search(EbookReq req){
-        return ebookMapper.selectByName("%"+req.getName()+"%");
+    public List<EbookResp> search(EbookReq req){
+        List<Ebook> ebookList  = ebookMapper.selectByName("%" + req.getName() + "%");
+        //将List<Ebook>转换为List<EbookResp>
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
