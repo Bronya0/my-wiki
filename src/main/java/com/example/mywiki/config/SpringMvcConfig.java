2801ca766d07b27e6929e76a3066304d53bd4423
Bronya0 <1335968147@qq.com>
2021-06-09 18:06:59 +0800

    3-8 使用aop打日志，注释过滤器、拦截器

--- src/main/java/com/example/mywiki/config/SpringMvcConfig.java
+++ src/main/java/com/example/mywiki/config/SpringMvcConfig.java
@@ -1,25 +1,25 @@
-package com.example.mywiki.config;
-
-import com.example.mywiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * 拦截器打印耗时
- * Created by tangssst@qq.com on 2021/06/09
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    @Resource
-    private LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-
-}
-
+//package com.example.mywiki.config;
+//
+//import com.example.mywiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * 拦截器打印耗时
+// * Created by tangssst@qq.com on 2021/06/09
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//    @Resource
+//    private LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//
+//}
+//
