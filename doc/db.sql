3913635a46e228b10b343f2baccf62a5c47011c0
Bronya0 <1335968147@qq.com>
2021-06-14 21:03:16 +0800

    4-5-1修改sql

--- doc/db.sql
+++ doc/db.sql
@@ -18,5 +18,25 @@ insert into `ebook` (id,name,description) values (2,'vue教程','前后端分离
 insert into `ebook` (id,name,description) values (3,'python教程','大众化而应用广泛的编程语言');
 insert into `ebook` (id,name,description) values (4,'mysql教程','最流行的开源数据库管理系统');
 insert into `ebook` (id,name,description) values (5,'IDEA教程','Java开发的首选集成开发环境');
-
-
+insert into `ebook` (id,name,description) values (6,'go教程','天生并发');
+insert into `ebook` (id,name,description) values (7,'语文教程','中国人不能不学中文');
+insert into `ebook` (id,name,description) values (8,'数学教程','中国人不能不学数学');
+insert into `ebook` (id,name,description) values (9,'英语教程','中国人也得学英语');
+insert into `ebook` (id,name,description) values (10,'物理教程','中国人也得学物理');
+insert into `ebook` (id,name,description) values (11,'化学教程','中国人也得学化学');
+insert into `ebook` (id,name,description) values (12,'生物教程','中国人也得学生物');
+insert into `ebook` (id,name,description) values (13,'政治教程','中国人也得学政治');
+insert into `ebook` (id,name,description) values (14,'美术教程','中国人也得学美术');
+insert into `ebook` (id,name,description) values (15,'建模教程','中国人也得学建模');
+insert into `ebook` (id,name,description) values (16,'特效教程','中国人也得学特效');
+insert into `ebook` (id,name,description) values (17,'文案教程','中国人也得学文案');
+insert into `ebook` (id,name,description) values (18,'策划教程','中国人也得学策划');
+insert into `ebook` (id,name,description) values (19,'测试教程','中国人也得学测试');
+insert into `ebook` (id,name,description) values (20,'开发教程','中国人也得学开发');
+insert into `ebook` (id,name,description) values (21,'竞技教程','中国人也得学竞技');
+insert into `ebook` (id,name,description) values (22,'演绎教程','中国人也得学演绎');
+insert into `ebook` (id,name,description) values (23,'ppt教程','中国人也得学ppt');
+insert into `ebook` (id,name,description) values (24,'word教程','中国人也得学word');
+insert into `ebook` (id,name,description) values (25,'office教程','中国人也得学office');
+insert into `ebook` (id,name,description) values (26,'wps教程','中国人也得学wps');
+insert into `ebook` (id,name,description) values (27,'建站教程','中国人也得学建站');
