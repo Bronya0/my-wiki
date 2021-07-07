0a51c0da201b90249232760ef9c04fde269d5ede
bronya0 <tangssst@qq.com>
2021-07-07 14:35:18 +0800

    7-8 集成redis

--- src/main/java/com/example/mywiki/request/PageReq.java
+++ src/main/java/com/example/mywiki/request/PageReq.java
@@ -3,7 +3,7 @@ package com.example.mywiki.request;
 import lombok.Data;
 
 import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * 分页请求参数封装
@@ -12,11 +12,11 @@ import javax.validation.constraints.NotEmpty;
 public class PageReq {
 
     //页码
-    @NotEmpty(message = "页码不能为空")
+    @NotNull(message = "页码不能为空")
     private int page;
 
     //条数，限制非空、最大数
-    @NotEmpty(message = "条数不能为空")
+    @NotNull(message = "条数不能为空")
     @Max(value = 1000,message = "最大查询数：1000")
     private int size;
 
--- src/main/java/com/example/mywiki/response/UserLoginResp.java
+++ src/main/java/com/example/mywiki/response/UserLoginResp.java
@@ -2,17 +2,22 @@ package com.example.mywiki.response;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 用户登录输出，保留id、登录名、昵称
  * Created by tangssst@qq.com on 2021/07/06
  */
 @Data
-public class UserLoginResp {
+public class UserLoginResp implements Serializable {
 
     private Long id;
+
+    private String token;
 
     private String loginName;
 
     private String name;
+
 
 }
